efd7b9b3c29a9d201f68db20dc73647c35ddfca5
korede <lateefolawuyi20@gmail.com>
2024-08-09 02:23:15 +0100

    itemssss

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -15,9 +15,9 @@ function Login() {
 
   const isAuthenticated = state.accessToken !== null;
 
-  if(isAuthenticated) {
-    return <Navigate to="/"/>
-  }
+  // if(isAuthenticated) {
+  //   return <Navigate to="/"/>
+  // }
 
   const redirect = useNavigate()
 
