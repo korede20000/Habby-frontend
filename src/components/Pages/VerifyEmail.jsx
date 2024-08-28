367d0d3b021493e84cc66ed3d7f5ab7b29a2c02c
korede <lateefolawuyi20@gmail.com>
2024-08-28 09:35:12 +0100

    tim

--- src/components/Pages/VerifyEmail.jsx
+++ src/components/Pages/VerifyEmail.jsx
@@ -1,41 +1,41 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 
 const VerifyEmail = () => {
-    const [message, setMessage] = useState('');
-    const [loading, setLoading] = useState(true);
-    const location = useLocation();
+    const [searchParams] = useSearchParams();
+    const token = searchParams.get("token");
+    const navigate = useNavigate();
     
     useEffect(() => {
-        const queryParams = new URLSearchParams(location.search);
-        const token = queryParams.get('token');
+        const verifyEmail = async () => {
+            try {
+                const res = await fetch(`https://habby-api.onrender.com/verify-email?token=${token}`);
+                const data = await res.json();
+
+                if (data === "Email verified successfully") {
+                    alert("Email verified successfully!");
+                    navigate("/login"); // Redirect to login page
+                } else {
+                    alert("Invalid or expired token");
+                    navigate("/"); // Redirect to home page
+                }
+            } catch (error) {
+                console.error("Error verifying email:", error);
+                alert("An error occurred. Please try again later.");
+            }
+        };
 
         if (token) {
-            // Make API request to verify the email
-            fetch(`https://habby-api.onrender.com/verify-email?token=${token}`)
-                .then(response => response.json())
-                .then(data => {
-                    setLoading(false);
-                    setMessage(data.message);
-                })
-                .catch(error => {
-                    setLoading(false);
-                    setMessage('An error occurred. Please try again later.');
-                });
-        } else {
-            setLoading(false);
-            setMessage('Invalid token.');
+            verifyEmail();
         }
-    }, [location.search]);
+    }, [token, navigate]);
 
     return (
-        <div>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <p>{message}</p>
-            )}
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-8">
+                <h2 className="text-2xl font-bold text-center text-orange-600 mb-6">Verifying your email...</h2>
+            </div>
         </div>
     );
 }
